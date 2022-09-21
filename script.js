1d5c3fe8ef38de551673f18bb5f73c865a68295f
sappymoon <vietjohnnydiep@gmail.com>
2022-09-21 16:15:22 -0500

    added slider that changes dimensions

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-let grid = document.getElementById('grid');
-let cols = 20;
-let rows = 20;
 
-grid.style.setProperty('--grid-rows', rows);
-grid.style.setProperty('--grid-columns', cols);
 
-    for(i = 0; i < rows * cols; i++){
+function update(){
+    let grid = document.getElementById('grid');
+    let sliderValue = document.getElementById('slider').value;
+    let gridParent = document.getElementsByClassName('grid-flex');
+
+    document.getElementById('dimensions').innerText = sliderValue + " x " + sliderValue;
+
+    grid.style.setProperty('--grid-rows', sliderValue);
+    grid.style.setProperty('--grid-columns', sliderValue);
+
+    for(i = 0; i < sliderValue * sliderValue; i++){
         let gridItem = document.createElement('div');
         grid.appendChild(gridItem).className = 'grid-item';
         gridItem.onmouseover = function(){
             this.style.backgroundColor = 'green';
         }
     }
+}
+
+
 
