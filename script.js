33d823d6517659c30b7af058ac548d462b5bb160
sappymoon <vietjohnnydiep@gmail.com>
2022-09-20 20:52:31 -0500

    added inputs to change grid dimensions, added way to dynamically change grid size

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let grid = document.getElementById('grid');
+let cols = 20;
+let rows = 20;
 
-    for(i = 1; i <= 256; i++){
+grid.style.setProperty('--grid-rows', rows);
+grid.style.setProperty('--grid-columns', cols);
+
+    for(i = 0; i < rows * cols; i++){
         let gridItem = document.createElement('div');
         grid.appendChild(gridItem).className = 'grid-item';
         gridItem.onmouseover = function(){
