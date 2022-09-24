60783f06d0169b7d43db21b35f28eab7f6efa0a1
sappymoon <vietjohnnydiep@gmail.com>
2022-09-24 18:55:05 -0500

    made slider functional, added clear grid button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-
-
-function update(){
+//changes grid based on slider value
+function changeGrid(){
     let grid = document.getElementById('grid');
     let sliderValue = document.getElementById('slider').value;
     let gridParent = document.getElementsByClassName('grid-flex');
+
+    grid.innerHTML = ''; //resets grid
 
     document.getElementById('dimensions').innerText = sliderValue + " x " + sliderValue;
 
@@ -14,10 +15,32 @@ function update(){
         let gridItem = document.createElement('div');
         grid.appendChild(gridItem).className = 'grid-item';
         gridItem.onmouseover = function(){
-            this.style.backgroundColor = 'green';
+            this.style.backgroundColor = 'black';
         }
     }
 }
+
+//creates defualt 16x16 grid
+function defaultGrid(){
+    let grid = document.getElementById('grid');
+    let sliderValue = document.getElementById('slider').value;
+    let gridParent = document.getElementsByClassName('grid-flex');
+
+    grid.style.setProperty('--grid-rows', 16);
+    grid.style.setProperty('--grid-columns', 16);
+
+    for(i = 0; i < 16 * 16; i++){
+        let gridItem = document.createElement('div');
+        grid.appendChild(gridItem).className = 'grid-item';
+        gridItem.onmouseover = function(){
+            this.style.backgroundColor = 'black';
+        }
+    }
+}
+
+defaultGrid();
+
+
 
 
 
